fc8f861db93937bf8c5ed775bf4c259c181742a8
Stefan Gretscher <sgretscher@celemony.com>
2021-02-26 17:57:10 +0100

    Added hotfix for premature calls to prepareToPlay() from VST3 and AU wrappers.

--- modules/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp
+++ modules/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp
@@ -2253,7 +2253,7 @@ public:
             host.loadFrom (hostContext);
 
         processContext.sampleRate = processSetup.sampleRate;
-        preparePlugin (processSetup.sampleRate, (int) processSetup.maxSamplesPerBlock);
+        preparePlugin (processSetup.sampleRate, (int) processSetup.maxSamplesPerBlock, false);
 
         return kResultTrue;
     }
@@ -2334,7 +2334,7 @@ public:
             allocateChannelListAndBuffers (channelListFloat,  emptyBufferFloat);
             allocateChannelListAndBuffers (channelListDouble, emptyBufferDouble);
 
-            preparePlugin (sampleRate, bufferSize);
+            preparePlugin (sampleRate, bufferSize, true);
         }
 
         return kResultOk;
@@ -3049,7 +3049,7 @@ public:
                                                         : AudioProcessor::singlePrecision);
         getPluginInstance().setNonRealtime (newSetup.processMode == Vst::kOffline);
 
-        preparePlugin (processSetup.sampleRate, processSetup.maxSamplesPerBlock);
+        preparePlugin (processSetup.sampleRate, processSetup.maxSamplesPerBlock, false);
 
         return kResultTrue;
     }
@@ -3475,12 +3475,20 @@ private:
              | kNeedTransportState;
     }
 
-    void preparePlugin (double sampleRate, int bufferSize)
+    // TODO JUCE_ARA preparePlugin() is called several times, not just from setActive() -
+    //               Calling setRateAndBufferSizeDetails() is ok at any of these times,
+    //               but actually calling prepareToPlay() shall only be done from setActive(),
+    //               otherwise it is not balanced properly and moreover some ARA configuration
+    //               will change while the plug-in already is prepared to play...!
+    //               As a temporary workaround, we've added a bool argument to track that.
+    //               Note that the AU_Wrapper has similar issues, albeit in less likely edge-cases...
+    void preparePlugin (double sampleRate, int bufferSize, bool callPrepareToPlay)
     {
         auto& p = getPluginInstance();
 
         p.setRateAndBufferSizeDetails (sampleRate, bufferSize);
-        p.prepareToPlay (sampleRate, bufferSize);
+        if (callPrepareToPlay)
+            p.prepareToPlay (sampleRate, bufferSize);
 
         midiBuffer.ensureSize (2048);
         midiBuffer.clear();
